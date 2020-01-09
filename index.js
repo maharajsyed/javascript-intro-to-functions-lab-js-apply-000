d8a0bebd28e0f352ac3730217afaaa2e42991c5e
Maharaj Syed <maharajsyed@yahoo.com>
2020-01-09 02:38:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,4 +23,8 @@ function sayHiToGrandma(string)
   if(string === string.toUpperCase()){
     return"YES INDEED!"
   }
+  if(string === "I love you u, Grandma."){
+    return"I love you, too."
+  }
 }
+
